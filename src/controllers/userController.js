125f81a15014a57150f86bacb0de953448b388be
Dedalino <kelsonpapelo18@gmail.com>
2022-10-05 13:20:49 +0100

    convert js to typescript

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,22 +1,3 @@
-const users = require('../database/User')
-const { v4: uuidv4 } = require('uuid');
 
-const createUser = (req, res) => {
-    const {name, email} = req.body
 
-    const User = {
-        id:uuidv4(),
-        name,
-        email
-    }
-   users.push(User)
-   delete User.id
-   return res.json(User)
 
-}
-
-const listUsers = (req,res) => {
-    return res.status(200).json(users)
-}
-
-module.exports = {createUser,listUsers}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import app from './server'
+
+app.listen(4444, () => {
+    console.log("server is running")
+})
